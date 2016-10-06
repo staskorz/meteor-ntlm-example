fb0fedb4dcdca3516e2a7faa1fe76a6ff1f8e543
staskorz <staskorz@gmail.com>
2016-10-06 11:36:23 +0300

    Remove helpers automatically created by the "meteor create" command

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
 
 import ntlmLogin from '/imports/client/ntlm-auth/login';
 
@@ -12,24 +11,6 @@ ntlmLogin(err => {
 
 
 import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-	// counter starts at 0
-	this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-	counter() {
-		return Template.instance().counter.get();
-	},
-});
-
-Template.hello.events({
-	'click button'(event, instance) {
-		// increment the counter when button is clicked
-		instance.counter.set(instance.counter.get() + 1);
-	},
-});
 
 
 Template.ntlmUser.helpers({
